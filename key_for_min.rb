d64447d83c68aee693ab2eceddec9138b256440f
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-22 16:23:45 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,13 @@
 
 def key_for_min_value(name_hash)
 
+lowest_key = nil
+lowest_value = 0;
+name_hash.each do |key, value|
+  if value < lowest_value
+    lowest_key = key
+    lowest_value = value
+end
+end
+lowest_key
 end
